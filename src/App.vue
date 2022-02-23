8f09c2e92d0857fba39fc89abd5b1eba1fd5363d
Eryk Fryderyk <grzeniu098@gmail.com>
2022-02-23 11:13:17 +0100

    basic setup App

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SearchInput/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchInput from './components/SearchInput.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SearchInput,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="search-input">
+    <h1>Search Input</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchInput',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
