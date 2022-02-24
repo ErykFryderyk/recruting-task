ae1fb29671394d18d469922ec24867154f1091f0
Eryk Fryderyk <grzeniu098@gmail.com>
2022-02-24 23:03:55 +0100

    make a component more simple

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -1,42 +1,19 @@
 <template>
   <div class="search-box">
     <label for="search">Search</label>
-    <select name="" id="" v-model="selectValue" @click="handleInput">
-      <option value="posts">Posts</option>
-      <option value="comments">Comments</option>
-      <option value="albums">Albums</option>
-    </select>
-    <input id="search" type="text" v-model="searchValue" @click="handleInput" />
-    <h1>{{ selectValue }}</h1>
+    <input id="search" type="text"
+      v-model="inputValue"
+      @input="$emit('show-value', $event.target.value)"
+    />
   </div>
 </template>
 
 <script>
-import axios from "axios";
-import debounce from "lodash.debounce";
-
-const API = "https://jsonplaceholder.typicode.com/";
-
 export default {
   name: 'SearchInput',
   data() {
     return {
-      searchValue: "",
-      selectValue: "",
-      results: [],
     };
-  },
-  methods: {
-    handleInput: debounce(function () {
-      axios
-        .get(`${API}${this.selectValue}/`)
-        .then((response) => {
-          console.log(response);
-        })
-        .catch((error) => {
-          console.log(error);
-        });
-    }, 1000),
   },
 }
 </script>
@@ -44,7 +21,7 @@ export default {
 <style scoped lang="scss">
 .search-box {
   width: 100%;
-  height: 200px;
+  // height: 200px;
   display: flex;
   flex-direction: column;
   justify-content: center;
