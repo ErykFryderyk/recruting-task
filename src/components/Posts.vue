e000952b0ad9e54d729dca427626cf2aec360f54
Eryk Fryderyk <grzeniu098@gmail.com>
2022-02-24 23:01:28 +0100

    create a component to show posts

---
+++ src/components/Posts.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <li v-for="item in posts" :key="item.id">
+      <h2>{{item.title}}</h2>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Posts',
+  props: {
+    posts: Object,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
