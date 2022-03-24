ddee91c31ae63d6f14ef980257847f35800d052d
Eryk Fryderyk <grzeniu098@gmail.com>
2022-03-24 22:26:49 +0100

    add new fontawesome icon

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrashCan } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTrashCan, faCalendarPlus} from '@fortawesome/free-solid-svg-icons'
 
 
-library.add(faTrashCan);
-
+library.add(faTrashCan, faCalendarPlus);
 
 
 createApp(App)
