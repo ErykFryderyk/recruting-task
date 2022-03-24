197d18ee2f1174e6fd7b16b4f6d753d664a3ec09
Eryk Fryderyk <grzeniu098@gmail.com>
2022-03-24 21:41:42 +0100

    create new component "MyHeader"

---
+++ src/components/MyHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="header">
+    <h1 class="header__title">Posts App</h1>
+    <div class="header__box">
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'MyHeader',
+    data() {
+      return{
+
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
