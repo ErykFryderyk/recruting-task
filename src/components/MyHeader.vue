884220002d4a0a7d43c758061517a86781b5583b
Eryk Fryderyk <grzeniu098@gmail.com>
2022-03-24 22:27:12 +0100

    basic style for component "MyHeader"

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="header">
-    <h1 class="header__title">Posts App</h1>
+    <h1 class="header__title">Posts App <font-awesome-icon icon="" /></h1>
     <div class="header__box">
+      <button class="add-new-post">
+        <font-awesome-icon class="add-new-post__icon" icon="calendar-plus" />
+      </button>
     </div>
   </div>
 </template>
@@ -18,5 +21,31 @@
 </script>
 
 <style lang="scss" scoped>
+.header{
+  padding-top:10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.add-new-post{
+  background-color: transparent;
+  padding: 3px;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  -webkit-tap-highlight-color: rgba(0,0,0,0);
 
+  &__icon{
+    font-size: 28px;
+    color: #357ce6;
+    transition: transform .1s ease-in-out;
+  }
+
+  &:hover {
+
+    .add-new-post__icon{
+      transform: scale(1.1);
+    }
+  }
+}
 </style>
