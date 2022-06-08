b786606506e97693a840ef22fd0b2f74cc7732ed
Eryk Fryderyk <grzeniu098@gmail.com>
2022-06-08 23:31:37 +0200

    add icons

--- src/main.js
+++ src/main.js
@@ -1,14 +1,26 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faTrashCan, faCalendarPlus, faXmark} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faTrashCan,
+  faCalendarPlus,
+  faXmark,
+  faAngleLeft,
+  faAnglesLeft,
+  faAngleRight,
+  faAnglesRight,
+} from "@fortawesome/free-solid-svg-icons";
 
+library.add(
+  faTrashCan,
+  faCalendarPlus,
+  faXmark,
+  faAnglesLeft,
+  faAngleLeft,
+  faAngleRight,
+  faAnglesRight
+);
 
-library.add(faTrashCan, faCalendarPlus, faXmark);
-
-
-createApp(App)
-  .component("font-awesome-icon", FontAwesomeIcon)
-  .mount('#app')
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app");
